b45483d522ab59a49d393059236debcdb5f3df29
이다영 <id46827@gmail.com>
2024-06-14 18:08:18 +0900

    init

---
+++ src/components/List.jsx
@@ -0,0 +1,62 @@
+import "./List.css";
+import ToDoItem from "./ToDoItem";
+import { useState, useMemo } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+  return (
+    <div className="List">
+      <h3>Todo List 🐤</h3>
+      <div>totalCount: {totalCount}</div>
+      <div>doneCount: {doneCount}</div>
+      <div>notDoneCount: {notDoneCount}</div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <ToDoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,37 @@
+import "./ToDoItem.css";
+import { memo } from "react";
+
+const ToDoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="ToDoItem">
+      <input
+        onChange={onChangeCheckbox}
+        readOnly
+        checked={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+// export default memo(ToDoItem, (prevProps, nextProps) => {
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+
+//   return true;
+// });
+
+export default memo(ToDoItem);
